1b434d3067c0d81a6ed57ac629faa63cde0616b1
Afreda323 <anthonyfreda323@gmail.com>
2017-12-13 21:28:15 -0500

    Layed out args for discovery

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a Koa and GraphQL backend that queries for movies from MovieDB.
 - Create a file called .env
 - Copy the .env.schema into it. 
 - Add your MovieDB API key.
-
-    npm install
-	npm start
+- npm install
+- npm start or npm watch
 
 ## TODO: 
-- Discover
+- Discover query
+- Add descriptions to all object types
 
 ## What may be added later
 - TV
--- src/graphql/RootQuery.js
+++ src/graphql/RootQuery.js
@@ -3,6 +3,9 @@ const {
   GraphQLString,
   GraphQLNonNull,
   GraphQLInt,
+  GraphQLFloat,
+  GraphQLUnionType,
+  inp,
 } = require('graphql')
 
 // MOVIE
@@ -14,7 +17,7 @@ const {
   popularResolver,
   topRatedResolver,
   upcomingResolver,
-  searchResolver
+  searchResolver,
 } = require('./resolvers/movieListResolver')
 const MovieListType = require('./types/MovieListType')
 const MoviePlusType = require('./types/MoviePlusType')
@@ -32,38 +35,61 @@ module.exports = new GraphQLObjectType({
     nowPlaying: {
       type: MovieListType,
       args: {
-        page: { type: GraphQLInt },
+        page: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve: nowPlayingResolver,
     },
     popular: {
       type: MovieListType,
       args: {
-        page: { type: GraphQLInt },
+        page: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve: popularResolver,
     },
     topRated: {
       type: MovieListType,
       args: {
-        page: { type: GraphQLInt },
+        page: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve: topRatedResolver,
     },
     upcoming: {
       type: MovieListType,
       args: {
-        page: { type: GraphQLInt },
+        page: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve: upcomingResolver,
     },
     search: {
       type: MoviePlusType,
       args: {
-        query: { type: GraphQLString },
-        page: { type: GraphQLInt },
+        query: { type: new GraphQLNonNull(GraphQLString) },
+        page: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve: searchResolver,
+    },
+    discover: {
+      type: MoviePlusType,
+      args: {
+        page: { type: new GraphQLNonNull(GraphQLInt) },
+        sortBy: {
+          type: GraphQLString,
+          description:
+            'Allowed Values: popularity.asc, popularity.desc, release_date.asc, release_date.desc, revenue.asc, revenue.desc, primary_release_date.asc, primary_release_date.desc, original_title.asc, original_title.desc, vote_average.asc, vote_average.desc, vote_count.asc, vote_count.desc',
+        },
+        primary_release_year: { type: GraphQLInt },
+        year: { type: GraphQLInt },
+        primary_release_date_gte: { type: GraphQLString },
+        primary_release_date_lte: { type: GraphQLString },
+        release_date_gte: { type: GraphQLString },
+        release_date_lte: { type: GraphQLString },
+        vote_count_gte: { type: GraphQLString },
+        vote_count_lte: { type: GraphQLString },
+        vote_average_gte: { type: GraphQLFloat },
+        vote_average_lte: { type: GraphQLFloat },
+      },
+      // include_video
+      // resolve: discoverResolver,
     },
   }),
 })
